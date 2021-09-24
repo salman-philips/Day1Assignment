bceb91d7a1f237465c777d2ed6d3d31b90c2f897
Mohmed Salman Siddiqi <salmansiddique897@gmail.com>
2021-09-24 11:42:18 +0530

    start activity for result

--- app/src/main/java/com/abrselmantutorials/day1assignment/HomeActivity.kt
+++ app/src/main/java/com/abrselmantutorials/day1assignment/HomeActivity.kt
@@ -1,15 +1,25 @@
 package com.abrselmantutorials.day1assignment
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
 import android.widget.TextView
 
 class HomeActivity : AppCompatActivity() {
-    lateinit var textView: TextView
+    lateinit var editText: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-        textView = findViewById(R.id.home_text_view)
-        textView.text = intent.getStringExtra("name")
+        editText = findViewById(R.id.home_text_view)
+    }
+
+    fun handleFinish(view: View) {
+        val data = editText.text.toString()
+        val intent=Intent()
+        intent.putExtra("data",data)
+        setResult(RESULT_OK,intent)
+        finish()
     }
 }
--- app/src/main/java/com/abrselmantutorials/day1assignment/MainActivity.kt
+++ app/src/main/java/com/abrselmantutorials/day1assignment/MainActivity.kt
@@ -1,58 +1,34 @@
 package com.abrselmantutorials.day1assignment
 
+
 import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
-import android.provider.AlarmClock
+
 import android.widget.Button
-import android.widget.EditText
+
 import android.widget.TextView
-import android.widget.Toast
+
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     lateinit var button: Button
-    lateinit var callButton: Button
-    lateinit var editText: EditText
     lateinit var textView: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         button = findViewById(R.id.button)
-        editText = findViewById(R.id.name)
         textView = findViewById(R.id.text)
-        callButton = findViewById(R.id.call)
         button.setOnClickListener {
-            goToHomeActivity()
-        }
-
-        callButton.setOnClickListener {
-            createAlarm("hi",11,12)
-        }
-
-
-    }
-
-    fun createAlarm(message: String, hour: Int, minutes: Int) {
-        val intent = Intent(AlarmClock.ACTION_SET_ALARM).apply {
-            putExtra(AlarmClock.EXTRA_MESSAGE, message)
-            putExtra(AlarmClock.EXTRA_HOUR, hour)
-            putExtra(AlarmClock.EXTRA_MINUTES, minutes)
-        }
-        if (intent.resolveActivity(packageManager) != null) {
-            startActivity(intent)
-        }
-    }
-
-
-        private fun goToHomeActivity() {
-        val nameFormEdit: String = editText.text.toString()
-        if (nameFormEdit.isNullOrBlank()) {
-            Toast.makeText(this@MainActivity, "please enter name", Toast.LENGTH_SHORT).show()
-        } else {
             val intent = Intent(this@MainActivity, HomeActivity::class.java)
-            intent.putExtra("name", nameFormEdit)
-            this@MainActivity.startActivity(intent)
+            startActivityForResult(intent, 123)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 123 && resultCode == RESULT_OK) {
+            val dataFromOtherActivity = data?.getStringExtra("data")
+            textView.text = dataFromOtherActivity
         }
     }
 }
