280d07b6b3c43cad6d6528ce33ea40fac66b86dd
Mohmed Salman Siddiqi <salmansiddique897@gmail.com>
2021-09-24 12:47:00 +0530

    Lifecycle of activity

--- app/src/main/java/com/abrselmantutorials/day1assignment/MainActivity.kt
+++ app/src/main/java/com/abrselmantutorials/day1assignment/MainActivity.kt
@@ -3,6 +3,7 @@ package com.abrselmantutorials.day1assignment
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
@@ -10,24 +11,35 @@ import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
-    lateinit var button: Button
-    lateinit var editText: EditText
-    lateinit var textView: TextView
+    val TAG = "SelmanTag"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        button = findViewById(R.id.button)
-        editText = findViewById(R.id.name)
-        textView = findViewById(R.id.text)
-        button.setOnClickListener {
-            val nameFormEdit: String = editText.text.toString()
-            if (nameFormEdit.isNullOrBlank()) {
-                Toast.makeText(this@MainActivity, "please enter name", Toast.LENGTH_SHORT).show()
-            } else {
-              val intent=Intent(this@MainActivity,HomeActivity::class.java)
-                intent.putExtra("name",nameFormEdit)
-                this@MainActivity.startActivity(intent)
-            }
-        }
+        Log.d(TAG, "onCreate")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy")
     }
 }
