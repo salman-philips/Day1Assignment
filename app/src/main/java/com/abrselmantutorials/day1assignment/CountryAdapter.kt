23c07b51e05634d31d4e74b361eb786c73580432
Mohmed Salman Siddiqi <salmansiddique897@gmail.com>
2021-09-27 11:42:15 +0530

    recyclerview onclick

--- app/src/main/java/com/abrselmantutorials/day1assignment/CountryAdapter.kt
+++ app/src/main/java/com/abrselmantutorials/day1assignment/CountryAdapter.kt
@@ -4,15 +4,31 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 
 class CountryAdapter(private val countryList: ArrayList<String>) :
     RecyclerView.Adapter<CountryAdapter.CountryViewHolder>() {
 
-    class CountryViewHolder(countryRowItem: View) : RecyclerView.ViewHolder(countryRowItem) {
+    inner class CountryViewHolder(countryRowItem: View) : RecyclerView.ViewHolder(countryRowItem),
+        View.OnClickListener {
         private val rowItemText = countryRowItem.findViewById(R.id.row_item_text) as TextView
         fun onBind(text: String) {
             rowItemText.text = text
+        }
+
+        init {
+            countryRowItem.setOnClickListener(this)
+        }
+
+        override fun onClick(v: View?) {
+            if (v != null) {
+                Toast.makeText(
+                    v.context,
+                    "Country clicked is ${countryList[adapterPosition]}",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
         }
     }
 
