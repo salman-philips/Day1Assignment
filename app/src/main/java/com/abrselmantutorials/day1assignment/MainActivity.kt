262957d10a77661c5a569852ff6b95a7f71ec3c2
Mohmed Salman Siddiqi <salmansiddique897@gmail.com>
2021-09-28 16:40:55 +0530

    Bind service

--- app/src/main/java/com/abrselmantutorials/day1assignment/MainActivity.kt
+++ app/src/main/java/com/abrselmantutorials/day1assignment/MainActivity.kt
@@ -1,14 +1,52 @@
 package com.abrselmantutorials.day1assignment
 
+import android.content.ComponentName
+import android.content.Context
 import android.content.Intent
+import android.content.ServiceConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.IBinder
 import android.view.View
 import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     private lateinit var intentForService: Intent
     private lateinit var editText: EditText
+    private var mBound: Boolean = false
+    private lateinit var mService: MusicService
+
+    /** Defines callbacks for service binding, passed to bindService()  */
+    private val connection = object : ServiceConnection {
+
+        override fun onServiceConnected(className: ComponentName, service: IBinder) {
+            // We've bound to LocalService, cast the IBinder and get LocalService instance
+            val binder = service as MusicService.LocalBinder
+            mService = binder.getService()
+            mBound = true
+        }
+
+        override fun onServiceDisconnected(arg0: ComponentName) {
+            mBound = false
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        Intent(this, MusicService::class.java).also { intent ->
+            bindService(intent, connection, Context.BIND_AUTO_CREATE)
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        unbindService(connection)
+        mBound = false
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -19,16 +57,14 @@ class MainActivity : AppCompatActivity() {
     fun serviceHandler(view: View) {
 
         when (view.id) {
-            R.id.start_service_button -> {
-                val number = editText.text.toString()
-                number.let {
-                    intentForService.putExtra(Constants.DATA_COUNTER, it.toInt())
-                    startService(intentForService)
+            R.id.bind_service_button -> {
+                if (mBound) {
+                    // Call a method from the LocalService.
+                    // However, if this call were something that might hang, then this request should
+                    // occur in a separate thread to avoid slowing down the activity performance.
+                    val num: Int = mService.add(2,3)
+                    Toast.makeText(this, "number: $num", Toast.LENGTH_SHORT).show()
                 }
-            }
-            R.id.stop_service_button -> {
-
-                stopService(intentForService)
             }
         }
     }
--- app/src/main/java/com/abrselmantutorials/day1assignment/MusicService.kt
+++ app/src/main/java/com/abrselmantutorials/day1assignment/MusicService.kt
@@ -2,6 +2,7 @@ package com.abrselmantutorials.day1assignment
 
 import android.app.Service
 import android.content.Intent
+import android.os.Binder
 import android.os.IBinder
 import android.util.Log
 import android.widget.Toast
@@ -11,8 +12,14 @@ import kotlin.properties.Delegates
 class MusicService : Service() {
     private var data by Delegates.notNull<Int>()
     private val TAG = this::class.java.simpleName
+
+
+    // Binder given to clients
+    private val binder = LocalBinder()
+
+
     override fun onBind(intent: Intent?): IBinder? {
-        return null
+        return binder
     }
 
     override fun onCreate() {
@@ -37,4 +44,15 @@ class MusicService : Service() {
         Toast.makeText(this, "Music download ended", Toast.LENGTH_SHORT).show()
         super.onDestroy()
     }
+
+    /**
+     * Class used for the client Binder.  Because we know this service always
+     * runs in the same process as its clients, we don't need to deal with IPC.
+     */
+    inner class LocalBinder : Binder() {
+        // Return this instance of LocalService so clients can call public methods
+        fun getService(): MusicService = this@MusicService
+    }
+
+    fun add(a: Int, b: Int) = a + b
 }
