c16860133560438294e6f3174eb8b0c5a7f0bb87
Mohmed Salman Siddiqi <salmansiddique897@gmail.com>
2021-09-28 12:18:13 +0530

    otp

--- app/src/main/java/com/abrselmantutorials/day1assignment/MainActivity.kt
+++ app/src/main/java/com/abrselmantutorials/day1assignment/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.abrselmantutorials.day1assignment
 
-import androidx.appcompat.app.AppCompatActivity
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val data: Uri? = intent?.data
+        data?.let { it
+            Toast.makeText(this@MainActivity, "Hi there", Toast.LENGTH_SHORT).show()}
     }
 }
